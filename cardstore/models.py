f8de24fcffef1ed63793e2ed25f70a63e64f8bf2
Dániel Rácz <daniel.racz@irob.uni-obuda.hu>
2020-08-15 21:50:27 +0200

    Loader script works!

--- cardstore/models.py
+++ cardstore/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
 
+class DeckMetaData(models.Model):
+    id_name = models.CharField(max_length=200, unique=True)
+    description = models.CharField(max_length=200)
+    official = models.BooleanField(default=False)
+    name = models.CharField(max_length=200)
+    icon = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.id_name} - {self.name}"
+
+
 class BlackCard(models.Model):
     text = models.CharField(max_length=200)
     icon = models.CharField(max_length=200)
@@ -18,14 +29,3 @@ class WhiteCard(models.Model):
 
     def __str__(self):
         return f"[{self.deck}] {self.text}"
-
-
-class DeckMetaData(models.Model):
-    description = models.CharField(max_length=200)
-    official = models.BooleanField()
-    name = models.CharField(max_length=200)
-    icon = models.CharField(max_length=200)
-
-    def __str__(self):
-        if self.official:
-            return f"[Official] [{self.name}]"
--- loader.py
+++ loader.py
@@ -1,30 +1,20 @@
-import json
-import requests
+# Note: just copy the content of this script tohe the manage.py shell terminal, it will populate the database
 
-from cardstore.models import WhiteCard, BlackCard, DeckMetaData
+import json
 
 with open('data.json', 'r') as file:
     data = json.load(file)
 
+from cardstore.models import DeckMetaData, WhiteCard, BlackCard
+for key in data['metadata'].keys():
+    print("Using {key}")
+    DeckMetaData.objects.create(id_name = key, **data['metadata'][key]).save()
 
 
-url = "http://localhost:8000"
-def publish_white_card(data):
-    full_url = f"{url}/api/whitecards/"
-    print(f"Posting WHITE CARD to :{full_url}, -> {data}")
-    r = requests.post(full_url, json=data)
-    print(r.status_code)
+for key in data['white']:
+    WhiteCard.objects.create(**key).save()
 
-def publish_black_card(data):
-    full_url = f"{url}/api/blackcards/"
-    print(f"Posting BLACK CARD to :{full_url}, -> {data}")
-    r = requests.post(full_url, json=data)
-    print(r.status_code)
+for key in data['black']:
+    BlackCard.objects.create(**key).save()
 
-
-for card in data['white'][0:50]:
-    WhiteCard.objects.create(**card).save()
-
-
-for card in data['black'][0:50]:
-    BlackCard.objects.create(**card).save()
+print("Finished!")
