05c26b8f268b46bf808def45f08a81c6ae14b3e9
Daniel Racz <daniel.racz@kuka.com>
2020-08-15 21:48:05 +0200

    FIX: Admin

--- cardstore/admin.py
+++ cardstore/admin.py
@@ -8,7 +8,14 @@ class CardAdmin(admin.ModelAdmin):
     ]
     list_display = ('text', 'deck', 'icon')
 
+class BlackCardAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['text', 'deck', 'icon']}),
+    ]
+    list_display = ('text', 'deck', 'icon', 'pick')
+
+    list_filter = ['pick', 'deck']
 
 admin.site.register(WhiteCard, CardAdmin)
-admin.site.register(BlackCard, CardAdmin)
+admin.site.register(BlackCard, BlackCardAdmin)
 admin.site.register(DeckMetaData)
