f2480e8da7ee2d5c9815c0c09cd33fe5bae8fc5b
Daniel Racz <daniel.racz@kuka.com>
2020-09-21 18:28:38 +0200

    allow *

--- onlinegame/settings.py
+++ onlinegame/settings.py
@@ -28,8 +28,8 @@ CORS_ALLOW_ALL_ORIGINS = True
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '10.0.2.2', 'localhost', '127.0.0.1', '10.36.60.241', '*']
 
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
